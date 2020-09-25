07a20423c857b400ef061f4eedd945eb24814937
Gisele Bartolo <giselebartolo@gmail.com>
2020-09-25 11:28:54 -0300

    Agregadas signaturas de métodos

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,4 +1,32 @@
-
 public class Jugador {
+private String nombre;
+private Mazo mazo;
+public Jugador(String nombre, Mazo mazo) {
+	this.nombre = nombre;
+	this.mazo = mazo;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public Mazo getMazo() {
+	return mazo;
+}
+public void setMazo(Mazo mazo) {
+	this.mazo = mazo;
+}
 
+public boolean tieneCarta() {
+	return false;
+}
+
+public boolean tieneTurno() {
+	return false;
+}
+
+public void pasarCartaAlFinal(Jugador j) {
+	
+}
 }
--- src/Mazo.java
+++ src/Mazo.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 
 public class Mazo {
-ArrayList<Carta>cartas;
+	ArrayList<Carta>cartas;
 
-public Mazo() {
-	cartas=new ArrayList<>();
+	public Mazo() {
+		cartas=new ArrayList<Carta>();
+	}
+
+	public ArrayList<Carta> getCartas() {
+		ArrayList<Carta> cartasCopia = new ArrayList<Carta>(); 
+		cartasCopia = (ArrayList)cartas.clone(); 
+		return cartasCopia;
+	}
+
+	public void setCartas(ArrayList<Carta> cartas) {
+		this.cartas = cartas;
+	}
+public void mezclarCartas() {
+	//buscar como hacer ramdom
 }
 
-public ArrayList<Carta> getCartas() {
-	return cartas;
-}
-
-public void setCartas(ArrayList<Carta> cartas) {
-	this.cartas = cartas;
-}
 
 }
